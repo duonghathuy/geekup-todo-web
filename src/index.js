bda777d5289374c08dc66dbc19171088bcdc8922
Hà Thuỳ Dương <hathuyduong47@gmail.com>
2024-05-09 10:15:04 +0700

    Update path, component in import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
+import TodoPage from './TodoPage';
 import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <App />
+    <TodoPage />
   </React.StrictMode>
 );
