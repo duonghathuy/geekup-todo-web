e8d1288c2099af73af00bf5c31a313e83be135e0
Hà Thuỳ Dương <hathuyduong47@gmail.com>
2024-05-09 10:14:02 +0700

    Change alt for Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <a href="https://geekup-todo-web.vercel.app/">
-                <img className="logo" src={'https://geekup.vn/Icons/geekup-logo-general.svg'} alt="logo"/>
+                <img className="logo" src={'https://geekup.vn/Icons/geekup-logo-general.svg'} alt="GEEK Up Logo"/>
             </a>
             <p>Todo App</p>
         </div>
